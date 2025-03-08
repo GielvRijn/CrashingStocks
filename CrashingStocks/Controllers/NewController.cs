0279c6a3e6531b599fec1e037ad6c50adf10e202
Giel <Giel_vanRijn@hotmail.com>
2025-03-08 17:34:42 +0100

    First controllers and webapi

---
+++ CrashingStocks/Controllers/NewController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CrashingStocks.Controllers
+{
+    [ApiController]
+    [Route("NewApi/[Controller]")]
+    public class NewController : ControllerBase
+    {
+        [HttpGet]
+        public string SayHii()
+        {
+            return "Hii Learners";
+        }
+    }
+}
---
+++ CrashingStocks/Controllers/SecondController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CrashingStocks.Controllers
+{
+    [ApiController]
+    [Route("SecondApi/[action]")]
+    public class SecondController : ControllerBase
+    {
+        [HttpGet]
+        public string NeverSayHii()
+        {
+            return "HelloLearners";
+        }
+    }
+}
